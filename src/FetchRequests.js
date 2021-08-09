fa9a3e97844ee7f85dea9c07a7ab53c14d673f7b
Yokoso <tomsolga90@gmail.com>
2021-08-10 00:30:09 +0200

    update button logic

--- src/FetchRequests.js
+++ src/FetchRequests.js
@@ -1,34 +1,43 @@
 import React, {useState, useEffect, useRef} from 'react';
 import Button from '@material-ui/core/Button';
-
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import {Container} from '@material-ui/core';
 
 export default function FetchRequests() {
     const [data, setData] = useState([]);
+    const [sendRequest, setSendRequest] = useState(false);
 
     useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await fetch("https://dev.mulwi.cloud/api/tag")
-                const result = await response.json()
-                setData(result)
+        if(sendRequest) {
+            async function fetchData() {
+                try {
+                    const response = await fetch("https://dev.mulwi.cloud/api/tag")
+                    const result = await response.json()
+                    setData(result)
+                }
+                catch(e) {
+                    console.error(e)
+                }
             }
-            catch(e) {
-                console.error(e)
-            }
+            fetchData();
+
+            setSendRequest(false);
         }
-        fetchData();
-    }, []);
+            
+    }, [sendRequest]);
 
     return (
-        <div>
-            <Button variant="contained" color="primary"  className="fetch-button">Get Data</Button>
-            <ul>
+        <Container>
+            <Button onClick={() => setSendRequest(true)} variant="contained" color="primary"  className="fetch-button">Get Data</Button>
+            <br />
+            <Grid container>
                 {data.map(data => (
-                    <li /* key={data} */>
-                        {data}
-                    </li>
+                    <Grid item /* key={data} */ xs={12} md={6} lg={4}>
+                        <Paper style={{ padding: 10 }}>{data}</Paper>
+                    </Grid>
                 ))}
-            </ul>
-        </div>
+            </Grid>
+        </Container>
     );
 }
